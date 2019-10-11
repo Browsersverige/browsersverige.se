5898ec6421021a696837943f10c20400616f5c13
Urban Sanden <hej@urre.me>
2019-10-11 15:12:09 +0200

    Add filter

--- src/app.js
+++ src/app.js
@@ -1,3 +1,5 @@
+import { reset } from 'ansi-colors'
+
 const browsers = document.querySelector('#browsers')
 const browserData = []
 
@@ -16,7 +18,7 @@ const pickIcon = browser => {
 		return 'edge'
 	}
 	if (browser.includes('Explorer')) {
-		return 'internet-explorer_9-11'
+		return 'explorer'
 	}
 	if (browser.includes('Chrome')) {
 		return 'chrome'
@@ -56,20 +58,73 @@ const renderResult = arr => {
 				.split(':', 2)[1]
 				.match(/\d+/)[0]
 
-			if (percentage >= 0.5)
+			if (percentage >= 0.25)
 				return `
-			<div class="browser ${pickIcon(browserVersion)}">
-				<img src="/images/${pickIcon(browserVersion)}.svg"/>
-				<h2>${percentage}<span>%</span></h2>
-				<h3>${browserVersion}</h3>
-
-
-			</div>
-			`
+					<div class="browser ${pickIcon(browserVersion)}" data-id="${pickIcon(
+					browserVersion
+				)}">
+						<img src="/images/${pickIcon(
+							browserVersion
+						)}.svg" alt=´${browserVersion}´ role="presentation"/>
+						<h2>${percentage}<span>%</span></h2>
+						<h3>${browserVersion}</h3>
+					</div>
+				`
 		})
 		.join('')
 
 	browsers.innerHTML = html
 }
 
+const checkBoxes = document.querySelectorAll('.filter li input')
+
+let choices = []
+
+const filter = () => {
+	for (let checkbox of checkBoxes) {
+		checkbox.addEventListener('click', checkFilters, false)
+	}
+}
+
+const removeChoice = (array, element) => {
+	return array.filter(el => el !== element)
+}
+
+const checkFilters = e => {
+	for (let checkbox of checkBoxes) {
+		if (e.target.id === 'reset') {
+			resetFilters()
+		}
+		if (checkbox.checked) {
+			choices.push(checkbox.value)
+			showResults()
+		} else {
+			choices = removeChoice(choices, checkbox.id)
+		}
+	}
+}
+
+const resetFilters = () => {
+	const items = document.querySelectorAll('.browser')
+	for (let checkbox of checkBoxes) {
+		checkbox.checked = false
+		choices = []
+		for (let item of items) {
+			item.classList.remove('hidden')
+		}
+	}
+}
+
+const showResults = () => {
+	const items = document.querySelectorAll('.browser')
+	for (let item of items) {
+		if (choices.includes(item.getAttribute('data-id'))) {
+			item.classList.remove('hidden')
+		} else {
+			item.classList.add('hidden')
+		}
+	}
+}
+
 getData()
+filter()
